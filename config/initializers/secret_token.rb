f2eac39e9e7b9da1686bf78b0a2fcbad9a460a76
jernsthausen <jernsthausen@gmail.com>
2013-02-21 21:17:14 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ErnsthausenHome::Application.config.secret_token = '16de449c3e0e2b84e3106559e6c91a6d3e4e55674eb9df0bea3a3c77d8db3c067541252b3aaabe21b7b97fcf35174b14ba8f603f26593a43e2aac270a8734520'
