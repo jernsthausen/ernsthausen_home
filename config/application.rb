f4931dc9d64a136d24c80c1a15b1274451d3fb2d
jernsthausen <jernsthausen@gmail.com>
2013-02-24 19:18:29 -0500

    PPR Fixed

--- config/application.rb
+++ config/application.rb
@@ -63,7 +63,7 @@ module ErnsthausenHome
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/app/assets/files"
+
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
